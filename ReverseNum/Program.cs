073798255751989b0e79b78d13371c404c048343
FacePunch1337 <rezol1337@gmail.com>
2022-02-09 20:17:00 +0200

    +

---
+++ ReverseNum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReverseNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+
+            int part1 = value /= 1;
+            part1 %= 10;
+
+            int part2 = value /= 10;
+            part2 %= 10;
+
+            int part3 = value /= 10;
+
+
+            Console.Write("Результат: ");
+            Console.Write(part1);
+            Console.Write(part2);
+            Console.Write(part3);
+            Console.Write('\n');
+
+
+
+        }
+    }
+}
